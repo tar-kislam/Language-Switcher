95150c0a463ae42b041f9e72482ef29f6854c806
Tarık İSLAM <64484604+tar-kislam@users.noreply.github.com>
2021-12-24 13:37:29 +0200

    Create locale.js

---
+++ locale.js
@@ -0,0 +1,15 @@
+var lang = {
+
+ "config": {
+        "default": "en"
+    },
+
+ "language": {
+        "en": {
+            "hello": "Hi",
+        }
+ },
+  "tr": {
+            "hello": "Merhaba",
+  }
+};
